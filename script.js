0d187b4aee1c54d3b17a766e55716866deefc317
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-31 17:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+const submit = document.getElementsByTagName("input")[0];
+submit.addEventListener('click', ()=>{
+	const value = document.getElementById("colorSelect").value;
+	document.getElementById(value).remove();
+})
